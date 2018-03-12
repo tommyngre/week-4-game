7e1b04165d6d60b2b85d256d4b6a9dba4412151d
unknown <tommyn.gre@gmail.com>
2018-03-11 20:43:35 -0400

    char bios

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+var mandy = {
+  bio: "Even before she conquered the realm, she was brilliant and she knew it. \
+  But she needed to let the world know. She had to... one-up them all.",
+
+}
+
+var tuan = {
+  bio: "Outwardly, he seemed humble and content enough: an earnest worker; a honest man. \
+  But his humility was a facade, and he intended make those pay who took it for granted.",
+
+}
+
+var mrsun = {
+  bio: "Ham & Cheese, as Tuan and Sonic were once known, quickly recognized Mr. Sun's value as an asset \
+  and took him under their wings. Thusly, the duo became a trio and the Dang Gang became a powerful organization,\
+  and perhaps the only viable threat to Mandy's supremacy.",
+
+}
+
+var sonic = {
+  bio: "He was the more outspoken of the duo he comprised with Tuan, and for that reason \
+  his ostensible \"sidekick,\" Tuan, escaped notice more often than not. Thus, while Sonic did not run \
+  the Dang Gang, he made it possible.",
+
+}
+
+// She had never not succeeded in subsequenly one-upping one who one-upped her.
